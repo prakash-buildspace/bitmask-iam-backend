9861bac251e32480b309f861176aedd754580a82
Prakash Vishwakarma <prakashvk2003@gmail.com>
2026-09-17 11:24:25 +0530

    feat: add modules root module and import into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/config.module.js';
 import { HealthModule } from './health/health.module.js';
+import { ModulesModule } from './modules/modules.module.js';
 
 @Module({
   imports: [
     AppConfigModule,
     HealthModule,
+    ModulesModule,
   ],
   controllers: [],
   providers: [],
